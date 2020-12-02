1c91faa176c16a64dcdd559988fbfa59bedecb96
Pontus Hjortskog <pontus.hjortskog@kvd.se>
2020-12-02 08:00:53 +0100

    Day2 part 2 done

--- day02/main.hs
+++ day02/main.hs
@@ -1,9 +1,3 @@
--- input =
---   [ "1-3 a: abcde",
---     "1-3 b: cdefg",
---     "2-9 c: ccccccccc"
---   ]
-
 type Rule = (Char, Int, Int)
 
 parseRule :: String -> Rule
@@ -22,6 +16,13 @@ isValid (c, min, max) str =
   where
     count = countLetter str c
 
+isValid2 :: Rule -> String -> Bool
+isValid2 (c, min, max) str =
+  (== 1) $ length $ filter (== c) [str !! m, str !! x]
+  where
+    m = min - 1
+    x = max - 1
+
 countLetter :: String -> Char -> Int
 countLetter str c = length $ filter (== c) str
 
@@ -29,8 +30,10 @@ main :: IO ()
 main = do
   filecontents <- readFile "input.txt"
   let input = lines filecontents
-  print
-    ( length $
-        filter (== True) $
-          map (uncurry isValid . (\x -> (parseRule x, parsePassword x))) input
+      part1 = show $ length (filter (== True) $ map (uncurry isValid . (\x -> (parseRule x, parsePassword x))) input)
+      part2 = show $ length (filter (== True) $ map (uncurry isValid2 . (\x -> (parseRule x, parsePassword x))) input)
+  putStrLn
+    ( "Part 1: " ++ part1 ++ "\n"
+        ++ "Part 2: "
+        ++ part2
     )
